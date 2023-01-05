863d0bb29d722e07aa5438feafe2faa546003b0c
Nagendravarma <89314534+Nagendramanthena@users.noreply.github.com>
2023-01-05 21:46:47 +0530

    Simplercode

--- Microsoft/day5/(MinimumDeletionstoMakeArrayDivisible).java
+++ Microsoft/day5/(MinimumDeletionstoMakeArrayDivisible).java
@@ -48,3 +48,29 @@ class Solution {
 
 
 Tc-O(NlogN);
+
+
+
+//easy code
+
+class Solution {
+       public int minOperations(int[] A, int[] numsDivide) {
+        int g = numsDivide[0], tmp;
+        for (int a : numsDivide) {
+            g = gcd(a,g);
+        }
+        Arrays.sort(A);
+        for (int i = 0; i < A.length && A[i] <= g; ++i)
+            if (g % A[i] == 0)
+                return i;
+        return -1;
+    }
+    
+    public int gcd(int a,int b){
+        
+        if(b == 0)return a;
+        
+        return gcd(b,a%b);
+    }
+
+}
