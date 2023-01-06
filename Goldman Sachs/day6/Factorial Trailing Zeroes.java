49008e7b2464e42f89e37117bbe9ed9e502dc679
Nagendravarma <89314534+Nagendramanthena@users.noreply.github.com>
2023-01-06 14:52:09 +0530

    Factorial Trailing Zeroes-->day6-->problem1-->code

---
+++ Goldman Sachs/day6/Factorial Trailing Zeroes.java
@@ -0,0 +1,18 @@
+link:https://leetcode.com/problems/factorial-trailing-zeroes/
+
+
+code
+
+
+:class Solution {
+    public int trailingZeroes(int n) {
+        int pow = 5;
+        int ans = 0;
+        
+        while(pow<=n){
+            ans += n/pow;
+            pow *=5;
+        }
+        return ans;
+    }
+}
