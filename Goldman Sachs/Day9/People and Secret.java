1ef3ce1256c512f87e274241f7b7d77d283037b3
Nagendravarma <89314534+Nagendramanthena@users.noreply.github.com>
2023-01-09 23:10:45 +0530

    Problem1 code

---
+++ Goldman Sachs/Day9/People and Secret.java
@@ -0,0 +1,27 @@
+link:
+https://leetcode.com/problems/invalid-transactions/
+
+
+
+code:
+
+class Solution {
+    int fr;
+    int mod =(int) Math.pow(10,9)+7;
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        fr = forget;
+        int dp[][] = new int[n+1][forget+1];
+        for(int r[]:dp)Arrays.fill(r,-1);
+        return 1+f(n-delay,delay,forget-delay,dp);
+    }
+    public int f(int n,int d,int forget,int dp[][]){
+        
+        if(n<=0)return 0;
+        if(forget==0)return -1;
+        if(dp[n][forget]!=-1)return dp[n][forget];
+        
+        
+        return dp[n][forget] = (1+f(n-1,d,forget-1,dp)%mod+f(n-d,d,fr-d,dp)%mod)%mod;
+        
+    }
+}
